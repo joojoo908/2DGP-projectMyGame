c839c98608a56c3fc534725f03c58e3e26be1867
joojoo908 <looloo8910@gmail.com>
2024-10-09 21:33:00 +0900

    캐릭터idle 추가 클릭시 애니메이션 변화

--- MyGame_movetest.py
+++ MyGame_movetest.py
@@ -3,38 +3,62 @@ from pico2d import *
 import math
 import random
 
+WIDTH, HEIGHT = 1280 , 1024
+click =False;
+
 class player:
+    global click
     def __init__(self):
         self.x,self.y = 500,500
         self.framex = 0
         self.framey = 10
+        self.state = 0  # 0:idle 1: move
         self.image = load_image('red_hood.png')
+        self.idle = load_image('red_hood_idle.png')
 
     def update(self):
-        self.framex = (self.framex+1)%12
-        if self.framex==0:
-            self.framey = (self.framey-1)%11
-            if self.framey==8:
-                self.framey=10
-                self.framex=1
+        if self.state==0:
+            self.framex = (self.framex+1)%18
+        elif self.state==1:
+            self.framex = (self.framex+1)%12
+            if self.framex==0:
+                self.framey = (self.framey-1)%11
+                if self.framey==8:
+                    self.framey=10
+                    self.framex=1
+        if click:
+            self.state=1
+        else:
+            self.state=0
         #self.x+=5
     def draw(self):
         size = 112
         #좌측
-        #self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'i',self.x,self.y,300,300)
+        if self.state==0:
+            self.idle.clip_composite_draw(self.framex*80, 0 ,80,80 ,0,'i',self.x+15,self.y-15,140,140)
+        elif self.state==1:
+            #self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'i',self.x,self.y,300,300)
+            self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'h',self.x,self.y,200,200)
         #우측
-        self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'h',self.x,self.y,300,300)
+        #self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'h',self.x,self.y,300,300)
         #self.image.clip_draw(self.frame*120 , 0, 120,300 , self.x,self.y)
 
 def handle_events():
     global move
     global key
     global up, down , r ,l
+    global x,y
+    global click
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:
             key = 0
+        elif event.type == SDL_MOUSEMOTION:
+            x,y = event.x , HEIGHT -1 -event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            click = not click;
+            print(x,y)
         elif event.type == SDL_KEYDOWN:
             key =True
             if event.key==SDLK_RIGHT:
@@ -80,7 +104,6 @@ def render_world():
     update_canvas()
 
 
-WIDTH, HEIGHT = 1280 , 1024
 open_canvas(WIDTH, HEIGHT)
 
 reset_world()
