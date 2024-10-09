e7533ca5a14b401d3d0be60208e7b9e140ae4d67
joojoo908 <looloo8910@gmail.com>
2024-10-09 23:32:25 +0900

    마우스 위치에 따른 좌우 변환

--- MyGame_movetest.py
+++ MyGame_movetest.py
@@ -5,18 +5,22 @@ import random
 
 WIDTH, HEIGHT = 1280 , 1024
 click =False;
+x,y =0,0
 
 class player:
     global click
+    global x,y
     def __init__(self):
         self.x,self.y = 500,500
         self.framex = 0
         self.framey = 10
         self.state = 0  # 0:idle 1: move
+        self.dire =0
         self.image = load_image('red_hood.png')
         self.idle = load_image('red_hood_idle.png')
 
     def update(self):
+        #------애니메이션---
         if self.state==0:
             self.framex = (self.framex+1)%18
         elif self.state==1:
@@ -30,15 +34,24 @@ class player:
             self.state=1
         else:
             self.state=0
+        if x>=self.x:
+            self.dire=0
+        else:
+            self.dire=1;
         #self.x+=5
     def draw(self):
         size = 112
         #좌측
         if self.state==0:
-            self.idle.clip_composite_draw(self.framex*80, 0 ,80,80 ,0,'i',self.x+15,self.y-15,140,140)
+            if self.dire == 0:
+                self.idle.clip_composite_draw(self.framex*80, 0 ,80,80 ,0,'i',self.x+15,self.y-15,140,140)
+            else:
+                self.idle.clip_composite_draw(self.framex*80, 0 ,80,80 ,0,'h',self.x-15,self.y-15,140,140)
         elif self.state==1:
-            #self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'i',self.x,self.y,300,300)
-            self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'h',self.x,self.y,200,200)
+            if self.dire == 0:
+                self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'h',self.x,self.y,200,200)
+            else:
+                self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'i',self.x,self.y,200,200)
         #우측
         #self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133 ,0,'h',self.x,self.y,300,300)
         #self.image.clip_draw(self.frame*120 , 0, 120,300 , self.x,self.y)
