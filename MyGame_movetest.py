5678e59e57b2f7731e74dab276221f2716a32c57
joojoo908 <looloo8910@gmail.com>
2024-10-12 08:48:54 +0900

    일정 범위 밖으로 나가기 방지

--- MyGame_movetest.py
+++ MyGame_movetest.py
@@ -20,7 +20,7 @@ class player:
     global x,y
     global mx,my
     def __init__(self):
-        self.x,self.y = 500,500
+        self.x,self.y = 0,0
         self.framex = 0
         self.framey = 10
         self.state = 0  # 0:idle 1: move
@@ -29,6 +29,7 @@ class player:
         self.idle = load_image('red_hood_idle.png')
 
     def update(self):
+        global viewX ,viewY
         #------애니메이션---
         if self.state==0: #idle
             self.framex = (self.framex+1)%18
@@ -44,38 +45,69 @@ class player:
         if len(self.x,self.y,mx,my) > speed:
             self.state=1
             self.dire = mx <= self.x
+            if math.cos(angle(self.x,self.y,mx,my))<0 :
+                if self.x <= viewX -400:
+                    viewX += speed*math.cos(angle(self.x,self.y,mx,my))
+            elif math.cos(angle(self.x,self.y,mx,my))>0 :
+                if self.x >= viewX +400:
+                    viewX += speed*math.cos(angle(self.x,self.y,mx,my))
+                    
+            if math.sin(angle(self.x,self.y,mx,my))<0 :
+                if self.y <= viewY -300:
+                    viewY += speed*math.sin(angle(self.x,self.y,mx,my))
+            elif math.sin(angle(self.x,self.y,mx,my))>0 :
+                if self.y >= viewY +300:
+                    viewY += speed*math.sin(angle(self.x,self.y,mx,my))
             self.x += speed*math.cos(angle(self.x,self.y,mx,my))
             self.y += speed*math.sin(angle(self.x,self.y,mx,my))
+            
         else:
+            self.x =mx
+            self.y =my
             self.state=0
+            #print(viewX,viewY)
         
     def draw(self):
         size = 112
         #좌측
         if self.state==0:
             if self.dire == 0:
-                self.idle.clip_composite_draw(self.framex*80, 0 ,80,80 ,0,'i', self.x+15, self.y-15 ,140,140)
+                self.idle.clip_composite_draw(self.framex*80, 0 ,80,80
+                                              ,0,'i',WIDTH//2-viewX+ self.x+15, HEIGHT//2 -viewY+ self.y-15 ,140,140)
             else:
-                self.idle.clip_composite_draw(self.framex*80, 0 ,80,80 ,0,'h', self.x-15, self.y-15 ,140,140)
+                self.idle.clip_composite_draw(self.framex*80, 0 ,80,80
+                                              ,0,'h',WIDTH//2-viewX+ self.x-15,HEIGHT//2 -viewY+ self.y-15 ,140,140)
         elif self.state==1:
             if self.dire == 0:
                 self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133
-                                               ,0,'h',self.x,self.y,200,200)
+                                               ,0,'h',WIDTH//2-viewX+self.x, HEIGHT//2 -viewY + self.y,200,200)
             else:
                 self.image.clip_composite_draw(self.framex*size, self.framey*133 ,size,133
-                                               ,0,'i',self.x,self.y,200,200)
+                                               ,0,'i',WIDTH//2-viewX+self.x, HEIGHT//2 -viewY + self.y,200,200)
 
                 
 class Ground:
-    def __init__(self,x,y):
+    global viewX , viewY
+    def __init__(self,x,y,tiletype,tilenum):
         self.x,self.y = x,y
+        self.frame=0
+        self.tiletype=tiletype
+        self.tilenum=tilenum
         self.image = load_image('Ground.png')
+        self.water = load_image('Water.png')
     def update(self):
+        self.frame = (self.frame+1)%8
         pass
     def draw(self):
          #self.image.draw(self.x,self.y)
-        self.image.clip_composite_draw(0*176 , 2*80, 176 ,80 ,0,'i',self.x,self.y,200,200)
-         #self.image.clip_draw(0*176//2 , 0*80, 176//4 ,80 , self.x,self.y)
+        if self.tiletype==0:
+            self.image.clip_composite_draw(0*176 +1*16 , 2*80 +3*16, 16 + 0*176 , 16+ 0*80
+                                           ,0,'i',WIDTH//2-viewX+ self.x+25 ,HEIGHT//2 -viewY + self.y+25 ,50,50)
+        elif self.tiletype==1:
+            self.water.clip_composite_draw(self.frame*176 +1*16 , 1*80 +3*16, 16 + 0*176 , 16+ 0*80 ,
+                                           0,'i', WIDTH//2-viewX+ self.x+25 ,HEIGHT//2 -viewY + self.y+25,50,50)
+    def printself(self):
+        print( '(',self.x,',',self.y, ',',self.tiletype, ',', self.tilenum, '),')
 
 def handle_events():
     global move
@@ -93,8 +125,8 @@ def handle_events():
             x,y = event.x , HEIGHT -1 -event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
             if event.button == SDL_BUTTON_LEFT:
-                mx ,my = x,y
-            
+                mx ,my = -WIDTH//2 + viewX + x, -HEIGHT//2 + viewY +y
+                #print(viewX,viewY)
         elif event.type == SDL_KEYDOWN:
             key =True
             if event.key==SDLK_RIGHT:
@@ -124,8 +156,8 @@ def reset_world():
     key=True
     world=[]
 
-    ground =Ground(500,500)
-    #world.append(ground)
+    ground =Ground(0,0,0,0)
+    world.append(ground)
 
     p1 = player()
     world.append(p1)
@@ -149,6 +181,7 @@ while key:
     handle_events()
     update_world()
     render_world()
+    #print(viewX,viewY)
     delay(0.05)
 
 close_canvas()
